cb6300376819ed613465dd48afa4e30d957f9e65
Amy312 <42759041+Amy312@users.noreply.github.com>
2022-09-27 01:44:39 -0400

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -19,13 +19,10 @@ void init() {
     
 
 
-void insertWord(string word)
-{
+void insertWord(string word){
     node *currentNode = trie;  
-    for (int i = 0; i < word.length(); i++)
-    {
-        if(currentNode->mapero.find(word[i]) == currentNode->mapero.end())
-        {
+    for (int i = 0; i < word.length(); i++){
+        if(currentNode->mapero.find(word[i]) == currentNode->mapero.end()){
             currentNode->mapero[word[i]] = new node();
         }
         currentNode = currentNode->mapero[word[i]];
