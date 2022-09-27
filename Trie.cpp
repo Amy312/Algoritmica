b323c05b6d31c810ba7cc25e8093e2604e9992b2
Amy312 <42759041+Amy312@users.noreply.github.com>
2022-09-27 01:03:58 -0400

    Update and rename Trie to Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -1,3 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+struct node {
+    char currentCharacter;       
+    bool isWord;
+   //  int priority = 0;            
+    struct node *children[27];  // [null,null,null,......,null]
+    node() {
+        isWord = false;
+    }
+}*trie; 
+
+// Inicializar 
+
+void init() {
+    trie = new node();  // Instanciar el objeto trie
+}
+    
+
+
 void insertWord(string word) {
     node *currentNode =  trie;  
     for (int i = 0; i< word.length(); i++) { 
